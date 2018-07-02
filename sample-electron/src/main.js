4224c612637c988edebbe56d5e367fc5074a9330
ics-ikeda <ikeda@ics-web.jp>
2018-07-02 18:31:57 +0900

    不要なモジュールを排除

--- sample-electron/src/main.js
+++ sample-electron/src/main.js
@@ -3,9 +3,6 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path');
-const url = require('url');
-
 // メインウィンドウ
 let mainWindow;
 
